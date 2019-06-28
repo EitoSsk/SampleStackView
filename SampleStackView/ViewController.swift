6f1636ddb15f11f95096b60bc40e5ba694d71627
EitoSsk <bulletman813@gmail.com>
2019-06-28 23:38:38 +0900

    viewの追加

--- SampleStackView/ViewController.swift
+++ SampleStackView/ViewController.swift
@@ -9,12 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var viewA: UIView!
+    @IBOutlet weak var viewB: UIView!
+    @IBOutlet weak var viewC: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func alignLeading(_ sender: Any) {
+        stackView.alignment = UIStackView.Alignment.leading
+    }
+    
+    @IBAction func alignCenter(_ sender: Any) {
+        stackView.alignment = UIStackView.Alignment.center
+    }
+    
+    @IBAction func alignTrailing(_ sender: Any) {
+        stackView.alignment = UIStackView.Alignment.trailing
+    }
+    
+    @IBAction func switchA(_ sender: UISwitch) {
+        viewA.isHidden = !sender.isOn
+    }
+    
+    @IBAction func switchB(_ sender: UISwitch) {
+        viewB.isHidden = !sender.isOn
+    }
+    
+    @IBAction func switchC(_ sender: UISwitch) {
+        viewC.isHidden = !sender.isOn
+    }
+    
 }
 
